813543ea839e364e93cdfa8d2f8cd73d4bc8b974
Satyam916 <satyamverma4444@gmail.com>
2024-10-06 19:36:52 +0530

    linkedlist

---
+++ dataStructure/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        linkedList ll=new linkedList();
+        ll.addFirst(30);
+        ll.addFirst(10);
+    }
+}
